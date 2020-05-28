6540b5689c5bf6f9056e12a7dd9e8d8d1e17d3b2
Nathan Strong <nathanstrong.fx@gmail.com>
2020-05-27 21:20:01 -0700

    Week 9

---
+++ twitter/.ipynb_checkpoints/twitter_test-checkpoint.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from dataclasses import asdict
+
+from twython import TwythonStreamer
+
+from twitter.register_twitter import RegisterTwitter
+
+
+class MyStreamer(TwythonStreamer):
+    tweets = []
+
+    def __init__(self):
+        super().__init__(*RegisterTwitter().credentials())
+
+    def on_success(self, data):
+        """
+        What do we do when Twitter sends us data?
+        Here data will be a Python dict representing a tweet.
+        """
+        # We only want to collect English-language tweets
+        if data.get('lang') == 'en':
+            self.tweets.append(data)
+            print(f"received tweet #{len(self.tweets)}")
+            print(data)
+
+        # Stop when we've collected enough
+        if len(self.tweets) >= 100:
+            self.disconnect()
+
+    def on_error(self, status_code, data):
+        print(status_code, data)
+
+
+if __name__ == '__main__':
+    twitter = MyStreamer()
+    twitter.statuses.filter(track='data')
